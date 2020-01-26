4ce0e2b77c380a1f58d68dcb8beb01590579757b
Mark Anderson <emer@emer.net>
2020-01-26 14:37:10 -0500

    emacs.d, powerline-shell, and zshrc updates

---
+++ emacs.d/elisp/init-looks.el
@@ -0,0 +1,6 @@
+(set-face-attribute 'default nil :font "JetBrains Mono 15")
+(set-frame-font "JetBrains Mono 15" nil t)
+;; Kill the worthless toolbar 
+(tool-bar-mode -1)
+
+(provide 'init-looks)
---
+++ emacs.d/init.el
@@ -0,0 +1,30 @@
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+(require 'init-melpa)
+(require 'init-looks)
+
+(setq ns-option-modifier 'meta)
+(setq ns-command-modifier 'super)
+(setq ns-funtion-modifer 'hyper)
+
+
+;; Name and Email
+(setq user-full-name "Mark Anderson")
+(setq user-mail-address "emer@emer.net")
+(setq calendar-latitude 38.9)
+(setq calendar-longitude -77.2)
+(setq calendar-location-name "McLean, VA")
+
+(when window-system (set-frame-size (selected-frame) 125 40))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (git))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
